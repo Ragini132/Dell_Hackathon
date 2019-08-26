8eeb9e91043768f052edeab16b5b82c9ec2d8410
Ragini Rani <46533864+Ragini132@users.noreply.github.com>
2019-08-27 01:16:57 +0530

    Add files via upload

---
+++ fireBase.js
@@ -0,0 +1,26 @@
+var app_firebase={};
+
+(function(){
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAGxNlhWa5Nt6k7RGCEPPZTo5wzs_qQE-c",
+    authDomain: "dell-f13fd.firebaseapp.com",
+    databaseURL: "https://dell-f13fd.firebaseio.com",
+    projectId: "dell-f13fd",
+    storageBucket: "dell-f13fd.appspot.com",
+    messagingSenderId: "617068971683",
+    appId: "1:617068971683:web:767d191dd2da211d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  app_fireBase = firebase;
+})()
+
+
+
+
+
+
+
